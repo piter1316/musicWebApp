8b236df23f53720d7bc90f2cbc594b92709a6d7c
piter1316 <piter_1316@wp.pl>
2018-07-10 11:26:57 +0200

    episode 24 completed

--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 def index(request):
     all_albums = Album.objects.all()
@@ -10,5 +10,17 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
 
-def favourite(request):
-    pass
+def favourite(request,album_id):
+    album = get_object_or_404(Album, pk=album_id)
+
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist()):
+        return render(request, 'music/detail.html',{
+            'album': album,
+            'error_message': 'Song not selected',
+        })
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
