1168b3a7d5b36c384715f24190ef7250d1b0bd64
piter1316 <piter_1316@wp.pl>
2018-07-11 10:54:09 +0200

    episode 30 completed

--- music/urls.py
+++ music/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     # /music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add')
 
 ]
