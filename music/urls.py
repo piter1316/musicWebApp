d2deee427abbed4898037b7fd2c8b0674fec2384
piter1316 <piter_1316@wp.pl>
2018-07-11 10:14:02 +0200

    episode 29 completed/ generic views

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,10 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/<album_id>/favourite/
-    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite')
+
 ]
